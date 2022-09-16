350049e4a53cb40059c4cb9bbcbdaf15c86ec6a2
daniel gutierrez <daniel_gutierrezn15@hotmail.com>
2022-09-15 22:43:38 -0300

    spinner

--- src/Containers/Item/index.jsx
+++ src/Containers/Item/index.jsx
@@ -1,21 +1,18 @@
-const  Item = ({ product } ) => {
+const Item = ({ product }) => {
     console.log(product)
-    return ( 
-        
-        
-        <div className="flex-row">
-          <section className="cart_desafio">
-            <article >
-                <img className="cart_hollow" src={product.image} alt="mini figure" />
-                <figcaption className="cart_hollow--info">
-                    <span className="title"> {product.product} <br /> <small className="description"> {product.category}</small> </span>
-                    <span className="price">${product.price}</span>
-                </figcaption>
-            </article>
-          </section>
-        </div>
-        
-     )
+    return (
+        <>
+            <section className="cart_desafio">
+                <article>
+                    <img className="cart_hollow" src={product.image} alt="mini figure" />
+                    <figcaption className="cart_hollow--info">
+                        <span className="title"> {product.product} <br /> <small className="description"> {product.category}</small> </span>
+                        <span className="price">${product.price}</span>
+                    </figcaption>
+                </article>
+            </section>
+        </>
+    )
 }
- 
-export default  Item
+
+export default Item
--- src/Containers/ItemListContainer/ItemListContainer.jsx
+++ src/Containers/ItemListContainer/ItemListContainer.jsx
@@ -2,24 +2,39 @@ import React from 'react'
 import ItemList from '../ItemList';
 import { products } from '../../assets/productos';
 import { customFetch } from '../../utils/customFetch';
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
 
 const ItemListContainer = () => {
 
     const [listProducts, setListProducts] = useState([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         customFetch(products)
-           .then(res => setListProducts(res) )
+            .then(res =>{
+                setLoading(false)
+                setListProducts(res)
+                
+            } )
     }, [])
 
-    
+
 
     return (
-       <>
-       <ItemList listProducts={listProducts} />
-       </>
+        <>
+            <div className=' flex-row contenedorMain '>
+                {
+                    loading ?
+                    <CircularProgress />
+                    :
+                    <ItemList listProducts={listProducts} />
+
+                }
+
+            </div>
+        </>
     );
 }
- 
+
 export default ItemListContainer;
